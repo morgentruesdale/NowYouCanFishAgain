d69a86387fb615b936060a38897537c9c7169872
stronwood <morgentruesdale@gmail.com>
2024-04-14 02:46:01 -0400

    Initial commit.

--- src/main/java/dev/morgentruesdale/nowyoucanfishagain/events/PokemonCatchEvent.java
+++ src/main/java/dev/morgentruesdale/nowyoucanfishagain/events/PokemonCatchEvent.java
@@ -3,7 +3,6 @@ package dev.morgentruesdale.nowyoucanfishagain.events;
 import com.pixelmonmod.pixelmon.api.events.FishingEvent;
 import dev.morgentruesdale.nowyoucanfishagain.config.Config;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
@@ -23,16 +22,13 @@ public class PokemonCatchEvent {
 
 		Player player = e.player;
 		Level level = player.level();
-
-		if (level.isClientSide) {
-			return;
-		}
-
-		ServerLevel serverLevel = (ServerLevel) level;
 		Random random = new Random();
-		serverLevel.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.NEUTRAL, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
-		if(config.getcatchMessage()!="false") {
-			e.player.sendSystemMessage(Component.literal(config.getcatchMessage()));
+
+		level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.NEUTRAL, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
+		if (!config.getcatchMessage().equalsIgnoreCase("false")) {
+			player.sendSystemMessage(Component.literal(config.getcatchMessage()));
 		}
+
 	}
+
 }
