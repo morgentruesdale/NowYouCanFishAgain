9b16ff572427e06cacca6bdac4207c17ac17550c
stronwood <morgentruesdale@gmail.com>
2024-04-15 00:55:20 -0400

    Fixed config changes not taking effect

--- src/main/java/dev/morgentruesdale/nowyoucanfishagain/events/PokemonCatchEvent.java
+++ src/main/java/dev/morgentruesdale/nowyoucanfishagain/events/PokemonCatchEvent.java
@@ -7,7 +7,6 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
